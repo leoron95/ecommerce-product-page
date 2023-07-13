166612317dc5051b0c1e52c7aad2cbf7b0a6f1bd
leobel rondon <leobel95@gmail.com>
2023-07-13 16:44:37 -0400

    Add context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import { StateContext } from "@/context/StateContext";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Component {...pageProps} />
+    </StateContext>
+  );
 }
