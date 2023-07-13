5373850e2e6089a125d5da40d858c42f3b4ca74d
leobel rondon <leobel95@gmail.com>
2023-07-13 16:38:18 -0400

    Create product data

---
+++ src/data.js
@@ -0,0 +1,21 @@
+export const product = {
+  id: 1,
+  company: "Sneaker company",
+  name: "Fall Limited Edition Sneakers",
+  description:
+    "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+  price: 250,
+  discount: 50,
+  images: [
+    "/images/image-product-1.jpg",
+    "/images/image-product-2.jpg",
+    "/images/image-product-3.jpg",
+    "/images/image-product-4.jpg",
+  ],
+  thumbnails: [
+    "/images/image-product-1-thumbnail.jpg",
+    "/images/image-product-2-thumbnail.jpg",
+    "/images/image-product-3-thumbnail.jpg",
+    "/images/image-product-4-thumbnail.jpg",
+  ],
+};
