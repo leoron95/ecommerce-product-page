7a26995d4d656c89300131295a0e5c656d169755
leobel rondon <leobel95@gmail.com>
2023-07-13 16:42:11 -0400

    Refactor ProductInfo component

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,42 +1,13 @@
-import React from "react";
-import { IconCart } from "./Icons";
-import Image from "next/image";
-import shoes from "@/images/image-product-1.jpg";
+import { ProductDetails } from "./ProductDetails";
+import { ProductImage } from "./ProductImage";
 
-export const ProductInfo = () => {
+export const ProductInfo = ({ product }) => {
   return (
-    <div className="grid w-full grid-cols-1 lg:gap-32 lg:pt-16 lg:grid-cols-2 lg:px-52">
-      <div className="w-fit">
-        <Image className="rounded-2xl" src={shoes} width={450} height={450} />
-      </div>
+    <div className="max-w-[1440px] mx-auto">
+      <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-10 xl:gap-20 pt-1 lg:px-10 lg:pt-16  xl:px-36  min-[1440px]:px-52 min-[1440px]:gap-32 ">
+        <ProductImage product={product} />
 
-      <div className="flex flex-col w-full pt-[60px]">
-        <p className="text-sm font-bold uppercase text-[#ff7d1a] tracking-wider">
-          sneaker company
-        </p>
-        <h1 className="text-5xl font-bold">Fall Limited Edition Sneakers</h1>
-
-        <p className="text-base leading-7">
-          These low-profile sneakers are your perfect casual wear companion.
-          Featuring a durable rubber outer sole, they’ll withstand everything
-          the weather can offer.
-        </p>
-
-        <div className="font-bold">
-          <span className="text-3xl">$125.00</span> <span>50%</span>
-          <p>$250.00</p>
-        </div>
-
-        <div className="flex flex-row w-full">
-          <div>
-            <button>-</button> 0 <button>+</button>
-          </div>
-
-          <button className="flex bg-[#ff7d1a] py-5 px-28  items-center rounded-lg">
-            <IconCart />
-            <span className="font-bold text-white">Add to cart</span>
-          </button>
-        </div>
+        <ProductDetails product={product} />
       </div>
     </div>
   );
