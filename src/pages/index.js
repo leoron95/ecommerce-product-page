35b0af232b09de3aa53b90bd1f0b432b47e70d5f
leobel rondon <leobel95@gmail.com>
2023-07-16 11:35:55 -0400

    Add document title and attribution

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { LightBox } from "@/components/LightBox";
 import { Kumbh_Sans } from "next/font/google";
 import { product } from "@/data";
 import { useStateContext } from "@/context/StateContext";
+import Head from "next/head";
 
 const KumbhSans = Kumbh_Sans({
   subsets: ["latin"],
@@ -15,12 +16,41 @@ const KumbhSans = Kumbh_Sans({
 export default function Home() {
   const { showLightBox } = useStateContext();
   return (
-    <main
-      className={`${KumbhSans.className} text-base font-normal w-full  min-h-screen relative`}
-    >
-      {showLightBox && <LightBox product={product} />}
-      <Header />
-      <ProductInfo product={product} />
-    </main>
+    <div>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/images/favicon-32x32.png" sizes="any" />
+        <title>ecommerce product page</title>
+      </Head>
+
+      <main
+        className={`${KumbhSans.className} text-base font-normal w-full  min-h-screen relative`}
+      >
+        {showLightBox && <LightBox product={product} />}
+        <Header />
+        <ProductInfo product={product} />
+
+        <footer className="absolute bottom-0 w-full">
+          <div className="flex justify-center">
+            <p className="text-sm sm:text-lg">
+              Challenge by{" "}
+              <a
+                className="font-bold text-[#ff7d1a]"
+                href="https://www.frontendmentor.io?ref=challenge"
+                target="_blank"
+              >
+                Frontend Mentor
+              </a>
+              . Coded by{" "}
+              <a className="font-bold text-[#ff7d1a]" href="#">
+                Leoron95
+              </a>
+              .
+            </p>
+          </div>
+        </footer>
+      </main>
+    </div>
   );
 }
